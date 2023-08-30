6e750ac18ecc608c082ad1febd0724be1005e241
Beauty Makuoa <91092770+Makuoa@users.noreply.github.com>
2023-08-30 14:52:14 +0200

    Updates

---
+++ CMPG323_Project_2/Authentication/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CMPG323_Project_2.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ CMPG323_Project_2/Authentication/Class.cs
@@ -0,0 +1,6 @@
+﻿namespace CMPG323_Project_2.Authentication
+{
+    public class Class
+    {
+    }
+}
